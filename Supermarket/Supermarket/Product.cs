f88a110d49f3bed6b7ca177450db5c4beb5232e2
Sebastian Posada Marín <sebasmarin1099@gmail.com>
2022-02-27 15:37:38 -0500

    Product, FixedPriceProduct & VariablePriceProduct Finish

---
+++ Supermarket/Supermarket/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Supermarket
+{
+    public abstract class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public float Tax { get; set; }
+        public abstract decimal ValueToPay();
+        public override string ToString()
+        {
+            return $"{Id} - {Description}" + 
+                $"\n\tPrice......: {$"{Price:C2}", 15}" +
+                $"\n\tTax:.......: {$"{Tax:P2}", 15}";
+        }
+    }
+}
---
+++ Supermarket/Supermarket/VariablePriceProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    public class VariablePriceProduct : Product
+    {
+        public string Measurement { get; set; }
+        public float Quantity { get; set; }
+        public override decimal ValueToPay()
+        {
+            return Price * (decimal)Quantity * (decimal)Tax + (Price * (decimal)Quantity);
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tMeasurement: {$"{Measurement}",15}" +
+                $"\n\tQuantity...: {$"{Quantity:N2}",15}" +
+                $"\n\tValue:.....: {$"{ValueToPay():C2}",15}";
+        }
+    }
+}
